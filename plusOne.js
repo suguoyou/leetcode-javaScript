c5c78f220327c1a627e8625c8ca8d3e70af2073f
suguoyou <suguoyou@126.com>
2022-11-21 22:54:01 +0800

    update leetcode

---
+++ plusOne.js
@@ -0,0 +1,18 @@
+// 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+
+// 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+
+// 你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+//
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  const number = BigInt(digits.join("")) + 1n;
+  return number.toString().split("");
+};
+
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
